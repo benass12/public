438841795ffc02699fc7b25fe1a973bb9f83218f
Benas Skirmantas <Benas Skirmantas>
2017-02-10 16:59:36 +0200

    Fridge commit change

--- src/itacademy/exception/Fridge.java
+++ src/itacademy/exception/Fridge.java
@@ -22,7 +22,7 @@ public class Fridge {
     public void take(String item) throws NoSuchItemException {
         boolean result = food.remove(item);
         if (!result) {
-            throw new NoSuchItemException(item + " not found in the fridge");
+            throw new NoSuchItemException(item + " not found ");
         }
     }
 }
